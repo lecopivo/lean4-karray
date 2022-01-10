4b9c8dbed9c0c67a39e0a38fb2673c45ee1cf98b
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-01-10 12:39:23 -0300

    update toolchain; change process order; extract function (#11)

--- examples/output.cpp
+++ examples/output.cpp
@@ -1,5 +1,5 @@
 #include <lean/lean.h>
 #include <math.h>
 #define external extern "C" LEAN_EXPORT
-external double c_sqrt_sqrt(double _uniq_2);
-external double c_sqrt_sqrt(double _uniq_2){return sqrt(sqrt(_uniq_2));}
+external double c_sqrt_sqrt(double _uniq_1);
+external double c_sqrt_sqrt(double _uniq_1){return sqrt(sqrt(_uniq_1));}
