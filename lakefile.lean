3c72df1a429fb550c8f21a5aeed78409870674ea
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-01-19 21:05:54 -0300

    updated mathlib and toolchain

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ package KArray {
   supportInterpreter := true
   dependencies := #[{
     name := `mathlib
-    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "089ae0a61756a84e383111da1f6697210456ed3a"
+    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "fe835a1"
   }]
 
 }
